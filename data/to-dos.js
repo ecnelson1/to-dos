47ef044693e5aede086bea72467b0c721b7c4d10
Eion Nelson <ecnelson1@live.com>
2021-03-01 14:21:48 -0800

    initial commit with updated data structure and table files

---
+++ data/to-dos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    todo: 'wash the dishes',
+    completed: false,
+    user_id: 1
+  }, 
+  {
+    todo: 'Rake the Leaves',
+    completed: false,
+    user_id: 1
+  }, 
+  {
+    todo: 'Mow the Lawn',
+    completed: false,
+    user_id: 1
+  }
+];
